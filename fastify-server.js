542eeac1310259b9564390f1b662746b2b5bea96
Peter Lau <superpeterlau@outlook.com>
2020-08-27 00:21:42 +0800

    listen to 0.0.0.0

--- fastify-server.js
+++ fastify-server.js
@@ -2,6 +2,7 @@ const build = require('./api/index')
 
 const app = build()
 
+// app.listen(process.env.PORT || 3000, '0.0.0.0', (err, address) => {
 app.listen(process.env.PORT || 3000, (err, address) => {
   if (err) {
     app.log.error(err)
