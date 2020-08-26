cb11bc596b79a4a623d7b08d39a1c7d73a897520
Peter Lau <superpeterlau@outlook.com>
2020-08-26 16:14:01 +0800

    init fastify

---
+++ fastify-server.js
@@ -0,0 +1,11 @@
+const build = require('./api/index')
+
+const app = build()
+
+app.listen(process.env.PORT || 3000, (err, address) => {
+  if (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+  app.log.info(`server listening on ${address}`)
+})
