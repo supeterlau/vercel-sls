f3ebd05984459ba5a58f6b52914676eeb755ead1
Peter Lau <superpeterlau@outlook.com>
2023-07-09 20:26:36 +0800

    new handler

--- api/node.js
+++ api/node.js
@@ -10,13 +10,13 @@ const build = () => {
   app.get("/", async (req, res) => {
     const { name = "World" } = req.query;
     req.log.info({ name }, "hello world");
-    return `Hello ${name} via fastify`;
+    res.send(`Hello ${name} via fastify`);
   });
 
   app.get("/check", async (req, res) => {
     const { name = "check check" } = req.query;
     req.log.info({ name }, "hello world");
-    return `Hello ${name} via fastify`;
+    res.send(`Hello ${name} via fastify`);
   });
 
   return app;
