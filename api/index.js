70cec9b632ef666409c1a14097541458b2f7ba25
Peter Lau <superpeterlau@outlook.com>
2023-07-10 18:09:32 +0800

    add more api

--- api/index.js
+++ api/index.js
@@ -1,5 +1,48 @@
-const app = require("express")();
-const { v4 } = require("uuid");
+import express from "express";
+// import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import cors from "cors";
+import { v4 } from "uuid";
+
+const app_secret = "8a183f86642b16881fa6f994203e75b6f903a1f6";
+const app_id = "2f99dddae9cec17300e9";
+
+const GITHUB_URL = "https://github.com/login/oauth/access_token";
+
+let app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, "../public")));
+
+app.use(cors({ credentials: true, origin: true }));
+
+app.get("/api/status", (req, res) => {
+  res.status(200).send({ status: "ok" });
+});
+
+const GITHUB_CALLBACK = "/api/auth/github/callback";
+
+// clien: preact/app
+app.get(GITHUB_CALLBACK, async (req, res) => {
+  let url = `${GITHUB_URL}?client_id=${app_id}&client_secret=${app_secret}&code=${req.query.code}`;
+  try {
+    let res = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    let data = await res.text();
+    console.log(data);
+    res.redirect(`http://localhost:5173?access_token=${data.access_token}`);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.get("/api", (req, res) => {
   const path = `/api/item/${v4()}`;
@@ -9,7 +52,6 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/api/item/:slug", (req, res) => {
-  console.log(req.params);
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
 });
